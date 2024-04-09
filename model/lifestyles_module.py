63cea5b08d9f97a10ad5bb1626471c3c7a7d4d6d
2050-Calculators <gino.baudry@epfl.ch>
2024-04-09 09:28:20 +0200

    refresh requirements.txt (SQLAlchemy)

---
+++ model/lifestyles_module.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import pickle
+import json
+import os
+import numpy as np
+
+from model.common.data_matrix_class import DataMatrix
+from model.common.constant_data_matrix_class import ConstantDataMatrix
+from model.common.io_database import read_database, read_database_fxa
+from model.common.auxiliary_functions import compute_stock, constant_filter, read_database_to_ots_fts_dict
+
+
