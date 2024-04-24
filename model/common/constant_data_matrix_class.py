32f5830e27e0b82a264ac585222383c92ae32a1b
edoardochiarotti <62812416+edoardochiarotti@users.noreply.github.com>
2024-04-24 09:32:16 +0200

    Update constant_data_matrix_class.py
    updating __repr__(self)

--- model/common/constant_data_matrix_class.py
+++ model/common/constant_data_matrix_class.py
@@ -36,16 +36,15 @@ class ConstantDataMatrix:
             self.idx = self.index_all()
             
     def __repr__(self):
-    
-        if len(self.col_labels) == 3:
+        
+        if len(self.col_labels) == 1:
             return f'ConstantDataMatrix with shape {self.array.shape} and variables {self.col_labels["Variables"]}'
-        if len(self.col_labels) == 4:
+        if len(self.col_labels) == 2:
             return f'ConstantDataMatrix with shape {self.array.shape}, variables {self.col_labels["Variables"]} and categories1 {self.col_labels["Categories1"]}'
-        if len(self.col_labels) == 5:
+        if len(self.col_labels) == 3:
             return f'ConstantDataMatrix with shape {self.array.shape}, variables {self.col_labels["Variables"]}, categories1 {self.col_labels["Categories1"]} and categories2 {self.col_labels["Categories2"]}'
-        if len(self.col_labels) == 6:
+        if len(self.col_labels) == 4:
             return f'ConstantDataMatrix with shape {self.array.shape}, variables {self.col_labels["Variables"]}, categories1 {self.col_labels["Categories1"]}, categories2 {self.col_labels["Categories2"]} and categories3 {self.col_labels["Categories3"]}'
-
 
     def read_data(self, constant, num_cat):
         dims = []
