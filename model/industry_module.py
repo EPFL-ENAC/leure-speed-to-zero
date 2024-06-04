2515730167282743523ebc8c2a265e3142aa915e
edoardochiarotti <62812416+edoardochiarotti@users.noreply.github.com>
2024-06-04 11:04:36 +0200

    Update industry_module.py

--- model/industry_module.py
+++ model/industry_module.py
@@ -1921,12 +1921,12 @@ def local_industry_run():
     # return
     return results_run
 
-# run local
-__file__ = "/Users/echiarot/Documents/GitHub/2050-Calculators/PathwayCalc/model/industry_module.py"
-database_from_csv_to_datamatrix()
-start = time.time()
-results_run = local_industry_run()
-end = time.time()
-print(end-start)
+# # run local
+# __file__ = "/Users/echiarot/Documents/GitHub/2050-Calculators/PathwayCalc/model/industry_module.py"
+# # database_from_csv_to_datamatrix()
+# start = time.time()
+# results_run = local_industry_run()
+# end = time.time()
+# print(end-start)
 
 
