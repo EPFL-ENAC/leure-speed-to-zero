add units when adding variables + fix units in power
there were TWh summed to GWh in power

model/common/auxiliary_functions.py
@@ -494,13 +494,13 @@ def cost(dm_activity, dm_price_index, cdm_cost, cost_type, baseyear = 2015, unit
     
     # include variables in cdm_cost inside dm_activity
     dm_activity = dm_activity.copy()
-    dm_activity.add(1, dim="Variables", col_label="ones", dummy=True)
+    dm_activity.add(1, dim="Variables", col_label="ones", dummy=True, unit='#')
     variables = cdm_cost.col_labels["Variables"]
     idx = dm_activity.idx
     idx_cdm = cdm_cost.idx
     # !FIXME: adjust keep_LE to work with constant instead of dm_activity
     arr_temp = (cdm_cost.array[idx_cdm['d-factor']] * dm_activity.array[:, :, idx["ones"], ...])
-    dm_activity.add(arr_temp[:, :, np.newaxis, ...], dim="Variables", col_label='d-factor')
+    dm_activity.add(arr_temp[:, :, np.newaxis, ...], dim="Variables", col_label='d-factor', unit='#')
     idx_temp = dm_activity.idx
     dm_activity.array[:, [idx_temp[y] for y in years_na], idx_temp['d-factor'], ...] = np.nan
 

model/minerals_module.py
@@ -332,7 +332,7 @@ def read_data(data_file):
     # return
     return DM_minerals, CDM_const
 
-def product_demand(DM_minerals, DM_buildings, DM_str, DM_tra, CDM_const):
+def product_demand(DM_minerals, DM_buildings, DM_pow, DM_tra, CDM_const):
     # get fxa
     DM_fxa = DM_minerals['fxa']
 
@@ -355,7 +355,7 @@ def product_demand(DM_minerals, DM_buildings, DM_str, DM_tra, CDM_const):
     # this is demand for installed capacity of batteries, expressed in kWh
 
     # get demand for batteries from energy sector
-    dm_battery = DM_str["battery"].copy()
+    dm_battery = DM_pow["battery"].copy()
 
     # convert from GW to kWh
     dm_battery.array = dm_battery.array * 1000000
@@ -410,33 +410,9 @@ def product_demand(DM_minerals, DM_buildings, DM_str, DM_tra, CDM_const):
 
     # this is the demand for energy that comes from different energy sources, expressed in GW (power)
 
-    # get fts
-    dm_energy_fts = DM_str["energy"].copy()
+    # Get new capacity installation
+    dm_energy = DM_pow["energy"].copy()
 
-    # add pvroof and pvutility
-    dm_energy_fts.operation('elc_energy-pvroof', "+", 'elc_energy-pvutility', dim="Variables", out_col="elc_pv",
-                            unit="GW", div0="error")
-    dm_energy_fts.drop(dim="Variables", col_label=['elc_energy-pvroof', 'elc_energy-pvutility'])
-
-    # deepen
-    dm_energy_fts.deepen()
-
-    # get ots, which are in fxa
-
-    dm_energy_ots = DM_fxa["elec_new"]
-
-    # sort
-    dm_energy_fts.sort(dim="Categories1")
-    dm_energy_ots.sort(dim="Categories1")
-
-    # add and save in dm_energy
-    arr_temp = dm_energy_ots.array + dm_energy_fts.array
-    dm_energy = dm_energy_ots.copy()
-    dm_energy.add(arr_temp, dim="Variables", col_label="product-demand", unit="GW")
-    dm_energy.drop(dim="Variables", col_label=['elc'])
-
-    # clean
-    del arr_temp, dm_energy_fts, dm_energy_ots
 
     ########################
     ##### PUT TOGETHER #####
@@ -563,7 +539,7 @@ def product_demand_split(DM_demand, dm_import, CDM_const):
     return DM_demand_split
 
 
-def mineral_demand_split(DM_minerals, DM_demand, DM_demand_split, CDM_const, DM_ind, DM_str):
+def mineral_demand_split(DM_minerals, DM_demand, DM_demand_split, CDM_const, DM_ind, DM_pow):
     # name of minerals
     minerals = ['aluminium', 'copper', 'graphite', 'lead', 'lithium', 'manganese', 'nickel', 'steel']
 
@@ -1049,8 +1025,11 @@ def mineral_demand_split(DM_minerals, DM_demand, DM_demand_split, CDM_const, DM_
     dm_energy_mindec.drop(dim="Categories1", col_label=energy)
     # not that here for example Austria 2020 for dir_energy_aluminium differs slightly from KNIME, supposedly for rounding differences (numbers are generally fine)
 
+    # !FIXME: there is a link between copper demand and electricity infrastructure,
+    # !FIXME: but here we are multiplying the total electricity demand by a constant
+    # that gives us the total copper-wire demand, and documentation for this is missing.
     # get electricity demand total (GWh) and constant for amount of copper in wires (kg/GWh)
-    dm_temp = DM_str["electricity-demand"]
+    dm_temp = DM_pow["electricity-demand"]
     cdm_temp = CDM_const["wire-copper"]
 
     # multiply direct demand times amount of copper in wires to get amount of copper in wires (kg)
@@ -1411,7 +1390,7 @@ def mineral_extraction(DM_minerals, DM_ind, dm_mindec, CDM_const):
 
     # make proportion of primary copper in industry
     dm_temp.add(dm_temp.array[:, :, idx2["ind_proportion_copper_secondary"]],
-                dim="Variables", col_label="ind_proportion_copper_primary")
+                dim="Variables", col_label="ind_proportion_copper_primary", unit='%')
 
     # adjust proportions of aluminium and copper
     dm_proportion.array[:, :, idx1["min_proportion_eu_aluminium_primary"]] = \
@@ -1429,9 +1408,9 @@ def mineral_extraction(DM_minerals, DM_ind, dm_mindec, CDM_const):
 
     # get proportions for hisarna and dri from temp into dm_proportion
     dm_proportion.add(dm_temp.array[:, :, idx2["ind_proportion_steel_hisarna"]], dim="Variables",
-                      col_label="min_proportion_eu_steel_hisarna")
+                      col_label="min_proportion_eu_steel_hisarna", unit='%')
     dm_proportion.add(dm_temp.array[:, :, idx2["ind_proportion_steel_hydrog-DRI"]], dim="Variables",
-                      col_label="min_proportion_eu_steel_DRI")
+                      col_label="min_proportion_eu_steel_DRI", unit='%')
 
     # fix dimensions of dm_production
     dm_production = dm_production.flatten()
@@ -1472,9 +1451,7 @@ def mineral_extraction(DM_minerals, DM_ind, dm_mindec, CDM_const):
     dm_extraction.add(np.nansum(dm_extraction.array, axis=-1, keepdims=True), dim="Categories2", col_label="total-sub")
     drops = ['BOF', 'DRI', 'EAF', 'hisarna', 'primary', 'secondary']
     dm_extraction.drop(dim="Categories2", col_label=drops)
-    dm_extraction.add(np.nansum(dm_extraction.array, axis=-3, keepdims=True), dim="Variables", col_label="total")
-    drops = ['abroad', 'home']
-    dm_extraction.drop(dim="Variables", col_label=drops)
+    dm_extraction.groupby({'total': '.*'}, dim='Variables', regex=True, inplace=True)
 
     # reshape
     dm_extraction = dm_extraction.flatten()
@@ -1887,46 +1864,51 @@ def simulate_industry_to_minerals_input():
     return DM_ind
 
 
-def simulate_storage_to_minerals_input():
+def simulate_power_to_minerals_input():
     
     dm = simulate_input(from_sector="storage", to_sector="minerals")
     
     # rename
     dict_rename = {
         "str_new-capacity_battery" : "str_energy-battery",
-        "elc_new-capacity_RES_other_geothermal" : "elc_energy-geo",
-        "elc_new-capacity_RES_other_hydroelectric" : "elc_energy-hydro",
-        "elc_new-capacity_RES_other_marine" : "elc_energy-marine",
-        "elc_new-capacity_RES_solar_Pvroof" : "elc_energy-pvroof",
-        "elc_new-capacity_RES_solar_Pvutility" : "elc_energy-pvutility",
-        "elc_new-capacity_RES_solar_csp" : "elc_energy-csp",
-        "elc_new-capacity_RES_wind_offshore" : "elc_energy-off-wind",
-        "elc_new-capacity_RES_wind_onshore" : "elc_energy-on-wind",
-        "elc_new-capacity_fossil_coal" : "elc_energy-coal",
-        "elc_new-capacity_fossil_gas" : "elc_energy-gas",
-        "elc_new-capacity_fossil_oil" : "elc_energy-oil",
-        "elc_new-capacity_nuclear" : "elc_energy-nuclear"
+        "elc_new-capacity_RES_other_geothermal" : "product-demand_energy-geo",
+        "elc_new-capacity_RES_other_hydroelectric" : "product-demand_energy-hydro",
+        "elc_new-capacity_RES_other_marine" : "product-demand_energy-marine",
+        "elc_new-capacity_RES_solar_Pvroof" : "product-demand_energy-pvroof",
+        "elc_new-capacity_RES_solar_Pvutility" : "product-demand_energy-pvutility",
+        "elc_new-capacity_RES_solar_csp" : "product-demand_energy-csp",
+        "elc_new-capacity_RES_wind_offshore" : "product-demand_energy-off-wind",
+        "elc_new-capacity_RES_wind_onshore" : "product-demand_energy-on-wind",
+        "elc_new-capacity_fossil_coal" : "product-demand_energy-coal",
+        "elc_new-capacity_fossil_gas" : "product-demand_energy-gas",
+        "elc_new-capacity_fossil_oil" : "product-demand_energy-oil",
+        "elc_new-capacity_nuclear" : "product-demand_energy-nuclear"
     }
 
     for k, v in dict_rename.items():
         dm.rename_col(k, v, dim='Variables')
+
+    dm.groupby({'product-demand_energy-pv': ['product-demand_energy-pvutility', 'product-demand_energy-pvroof']},
+               dim='Variables', inplace=True)
         
-    DM_str = {}
+    DM_pow = {}
     
     # battery
-    DM_str["battery"] = dm.filter_w_regex({"Variables" : ".*battery.*"})
+    DM_pow["battery"] = dm.filter_w_regex({"Variables" : ".*battery.*"})
     
     # energy
     energy = ['energy-coal', 'energy-csp', 'energy-gas', 'energy-geo', 'energy-hydro',
               'energy-marine', 'energy-nuclear', 'energy-off-wind', 'energy-oil',
-              'energy-on-wind', 'energy-pvroof', 'energy-pvutility']
-    find = ["elc_" + i for i in energy]
-    DM_str["energy"] = dm.filter({"Variables": find})
+              'energy-on-wind', 'energy-pv']
+    find = ["product-demand_" + i for i in energy]
+    dm_new_capacity = dm.filter({"Variables": find})
+    dm_new_capacity.deepen()
+    DM_pow["energy"] = dm_new_capacity
     
     # electricity
-    DM_str["electricity-demand"] = dm.filter({"Variables" : ["elc_electricity-demand_total"]})
+    DM_pow["electricity-demand"] = dm.filter({"Variables" : ["elc_electricity-demand_total"]})
 
-    return DM_str
+    return DM_pow
 
 
 def simulate_buildings_to_minerals_input():
@@ -2087,13 +2069,13 @@ def minerals(interface=Interface(), calibration=False):
 
     # storage
     if interface.has_link(from_sector='power', to_sector='minerals'):
-        DM_str = interface.get_link(from_sector='power', to_sector='minerals')
+        DM_pow = interface.get_link(from_sector='power', to_sector='minerals')
     else:
         if len(interface.list_link()) != 0:
             print('You are missing power to minerals interface')
-        DM_str = simulate_storage_to_minerals_input()
-        for i in DM_str.keys():
-            DM_str[i] = DM_str[i].filter({'Country': cntr_list})
+        DM_pow = simulate_power_to_minerals_input()
+        for i in DM_pow.keys():
+            DM_pow[i] = DM_pow[i].filter({'Country': cntr_list})
 
     # buildings
     if interface.has_link(from_sector='buildings', to_sector='minerals'):
@@ -2124,7 +2106,7 @@ def minerals(interface=Interface(), calibration=False):
         dm_ccus.filter({'Country': cntr_list}, inplace=True)
 
     # get product demand
-    DM_demand = product_demand(DM_minerals, DM_buildings, DM_str, DM_tra, CDM_const)
+    DM_demand = product_demand(DM_minerals, DM_buildings, DM_pow, DM_tra, CDM_const)
 
     # get product import
     dm_import = product_import(DM_ind)
@@ -2133,7 +2115,7 @@ def minerals(interface=Interface(), calibration=False):
     DM_demand_split = product_demand_split(DM_demand, dm_import, CDM_const)
 
     # get mineral demand split
-    dm_mindec, dm_mindec_sect = mineral_demand_split(DM_minerals, DM_demand, DM_demand_split, CDM_const, DM_ind, DM_str)
+    dm_mindec, dm_mindec_sect = mineral_demand_split(DM_minerals, DM_demand, DM_demand_split, CDM_const, DM_ind, DM_pow)
 
     # calibration
     if calibration is True:

model/power_module.py
@@ -421,6 +421,11 @@ def simulate_industry_to_power_input():
     df = pd.read_excel(f, sheet_name="default")
     dm = DataMatrix.create_from_df(df, num_cat=0)
 
+    # Units from TWh to GWh
+    dm.array = dm.array*1000
+    for var in dm.col_labels['Variables']:
+        dm.units[var] = 'GWh'
+
     # Space heating flow:
     dm_ind_electricity = dm.filter_w_regex({'Variables': 'ind_energy-demand_electricity'})
     dm_ind_hydrogen = dm.filter_w_regex({'Variables': 'ind_energy-demand_hydrogen'})
@@ -443,9 +448,15 @@ def simulate_ammonia_to_power_input():
     df = pd.read_excel(f, sheet_name="default")
     dm = DataMatrix.create_from_df(df, num_cat=0)
 
+    # From TWh to GWh
+    dm.array = dm.array*1000
+    for var in dm.col_labels['Variables']:
+        dm.units[var] = 'GWh'
+
     # Space heating flow:
     dm_amm_electricity = dm.filter_w_regex({'Variables': 'amm_energy-demand_electricity'})
     dm_amm_hydrogen = dm.filter_w_regex({'Variables': 'amm_energy-demand_hydrogen'})
+
 
     DM_ammonia = {
         'electricity': dm_amm_electricity,
@@ -466,6 +477,8 @@ def simulate_agriculture_to_power_input():
 
     # Space heating flow:
     dm_agr_electricity = dm.filter_w_regex({'Variables': 'agr_energy-demand_electricity'})
+    dm_agr_electricity.array = dm_agr_electricity.array*1000
+    dm_agr_electricity.units['agr_energy-demand_electricity'] = 'GWh'
 
     return dm_agr_electricity
 
@@ -671,6 +684,12 @@ def hourly_production_workflow(dm_production_np, dm_production_p, DM_production_
 def yearly_demand_workflow(DM_bld, dm_ind_electricity, dm_amm_electricity, dm_agr_electricity, DM_tra,
                            dm_ind_hydrogen, dm_amm_hydrogen):
 
+    def check_unit(dm, unit):
+        for var in dm.col_labels['Variables']:
+            if dm.units[var] != unit:
+                raise ValueError(f'variable {var} does not have unit {unit}')
+
+
     #########################################################################
     # CalculationLeafs - Electricity demand - Appliances [GWh]
     #########################################################################
@@ -696,7 +715,6 @@ def yearly_demand_workflow(DM_bld, dm_ind_electricity, dm_amm_electricity, dm_ag
     dm_tra_elec = DM_tra['electricity']
     dm_demand_train = dm_tra_elec.filter_w_regex({'Variables': '.*rail'})
     dm_demand_road = dm_tra_elec.filter_w_regex({'Variables': '.*road'})
-
     #############################################################################
     # CalculationLeafs - Electricity demand - Other sectors [GWh] (no-profiles)
     #############################################################################
@@ -708,20 +726,19 @@ def yearly_demand_workflow(DM_bld, dm_ind_electricity, dm_amm_electricity, dm_ag
     dm_demand_other.append(dm_ind_electricity, dim='Variables')
     dm_demand_other.append(dm_amm_electricity, dim='Variables')
 
-    ay_total = np.nansum(dm_demand_other.array[...], axis=-1)
-    dm_demand_other.add(ay_total, dim='Variables', col_label='total-other')
 
+    ay_total = np.nansum(dm_demand_other.array[...], axis=-1)
+    dm_demand_other.add(ay_total, dim='Variables', col_label='total-other', unit='GWh')
     #############################################################################
     # CalculationLeafs - Electricity demand - Hydrogen [GWh]
     #############################################################################
 
     dm_demand_hydrogen = DM_tra['hydrogen'].copy()
-
     dm_demand_hydrogen.append(dm_amm_hydrogen, dim='Variables')
     dm_demand_hydrogen.append(dm_ind_hydrogen, dim='Variables')
 
     ay_total = np.nansum(dm_demand_hydrogen.array[...], axis=-1)
-    dm_demand_hydrogen.add(ay_total, dim='Variables', col_label='total-hydrogen')
+    dm_demand_hydrogen.add(ay_total, dim='Variables', col_label='total-hydrogen', unit='GWh')
 
     #############################################################################
     # CalculationLeafs - Output Matrix Yearly Demand
@@ -736,6 +753,10 @@ def yearly_demand_workflow(DM_bld, dm_ind_electricity, dm_amm_electricity, dm_ag
         'space-cooling': dm_bld_cooling,
         'space-heating': dm_bld_heating
     }
+
+    for key in DM_yearly_demand:
+        dm = DM_yearly_demand[key]
+        check_unit(dm, 'GWh')
 
     return DM_yearly_demand
 
@@ -827,12 +848,12 @@ def hourly_demand_workflow(DM_yearly_demand, DM_demand_profiles, baseyear):
     ######################################
 
     # Yearly demand "other" with no profiles
-    dm_yearly_demand_other = DM_yearly_demand['other-profile']
+    dm_yearly_demand_other = DM_yearly_demand['other-profile'].copy()
     dm_yearly_demand_hydrogen = DM_yearly_demand['hydrogen-profile']
     dm_yearly_demand_other.append(dm_yearly_demand_hydrogen, dim='Variables')
     dm_yearly_demand_other = dm_yearly_demand_other.filter({'Variables':['total-hydrogen','total-other']})
     ay_yearly_demand_other = dm_yearly_demand_other.array[...].sum(axis=-1)
-    dm_yearly_demand_other.add(ay_yearly_demand_other, dim='Variables', col_label='grand-total-other')
+    dm_yearly_demand_other.add(ay_yearly_demand_other, dim='Variables', col_label='grand-total-other', unit='GWh')
 
     # Hourly profiles aggregation
     ay_hourly_profile_total = np.sum(dm_profile_hourly.array[...],axis=2)
@@ -916,6 +937,12 @@ def pow_refinery_interface(dm_gross_production):
 
     return dm_fossil_production
 
+
+def pow_minerals_interface(dm_new_capacity, DM_yearly_demand):
+    # Sum over all weeks, days, daily-hours
+
+    return
+
 #######################################################################################################################
 # CoreModule - Power
 #######################################################################################################################
@@ -994,7 +1021,7 @@ def power(lever_setting, years_setting, interface=Interface()):
     dm_gross_production = dm_capacity.filter({'Variables': ['pow_gross-yearly-production']})
     dm_emissions = emissions_workflow(dm_gross_production.copy(), cdm_const['emission-factors'])
 
-    dm_hourly_demand = hourly_demand_workflow(DM_yearly_demand, DM_demand_profiles, baseyear)
+    dm_hourly_demand = hourly_demand_workflow(DM_yearly_demand.copy(), DM_demand_profiles, baseyear)
 
     dm_hourly_equilibrium = storage_workflow(dm_hourly_demand, dm_hourly_production)
 
@@ -1004,6 +1031,8 @@ def power(lever_setting, years_setting, interface=Interface()):
     interface.add_link(from_sector='power', to_sector='oil-refinery', dm=dm_refinery)
     # same number of arg than the return function
 
+    dm_new_capacity = dm_capacity.filter({'Variables': ['pow_gross-yearly-production']})
+    # dm_minerals = pow_minerals_interface(dm_new_capacity, DM_yearly_demand)
     # concatenate all results to df
     results_run = dm_capacity
 