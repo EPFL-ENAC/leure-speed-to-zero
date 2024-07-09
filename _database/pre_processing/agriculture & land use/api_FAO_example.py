55658502b7d28e7ed14d0b9fccbfb314760fcfa2
acrosnier <156104914+acrosnier@users.noreply.github.com>
2024-07-09 17:43:54 +0200

    Agr Data SSR lever update

--- _database/pre_processing/agriculture & land use/api_FAO_example.py
+++ _database/pre_processing/agriculture & land use/api_FAO_example.py
@@ -2,11 +2,18 @@ import faostat
 # README on how to use faostat available at
 # Step 1. Install faostat package
 # https://pypi.org/project/faostat/
+
+list_countries = ['Austria', 'Belgium', 'Bulgaria', 'Croatia', 'Cyprus', 'Czechia', 'Denmark',
+                     'Estonia', 'Finland', 'France', 'Germany', 'Greece', 'Hungary', 'Ireland', 'Italy', 'Latvia',
+                     'Lithuania', 'Luxembourg', 'Malta', 'Netherlands (Kingdom of the)', 'Poland', 'Portugal', 'Romania', 'Slovakia',
+                     'Slovenia', 'Spain', 'Sweden', 'Switzerland', 'United Kingdom of Great Britain and Northern Ireland']
+
 ld = faostat.list_datasets()
 code = 'FBS'
 pars = faostat.list_pars(code)
+my_countries = [faostat.get_par(code, 'area')[c] for c in list_countries]
 my_pars = {
-    'area': faostat.get_par(code, 'area')['Switzerland'],
+    'area': my_countries,
     'element': faostat.get_par(code, 'elements')['Production Quantity'],
     'item': faostat.get_par(code, 'item')['Wheat and products']
 }
