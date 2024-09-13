0af17aad710d721a2d0e5445e9df2c6ccba8c82b
PaolaLeure <124268758+PaolaLeure@users.noreply.github.com>
2024-09-13 18:05:34 +0200

    Update lifestyles_preprocessing_EU.py
    household-size ots and fts

--- _database/pre_processing/lifestyles/Europe/lifestyles_preprocessing_EU.py
+++ _database/pre_processing/lifestyles/Europe/lifestyles_preprocessing_EU.py
@@ -3,35 +3,13 @@ import numpy as np
 import eurostat
 import pandas as pd
 from model.common.data_matrix_class import DataMatrix
-from model.common.auxiliary_functions import linear_fitting
-from model.common.io_database import update_database_from_dm
+from model.common.auxiliary_functions import linear_fitting, linear_forecast_BAU, moving_average, create_years_list
+from model.common.io_database import update_database_from_dm, csv_database_reformat, read_database_to_dm
 from _database.pre_processing.api_routine_Eurostat import get_data_api_eurostat
 
 EU27_cntr_list = ['Austria', 'Belgium', 'Bulgaria', 'Croatia', 'Cyprus', 'Czech Republic', 'Denmark', 'Estonia', 'Finland',
                   'France', 'Germany', 'Greece', 'Hungary', 'Ireland', 'Italy', 'Latvia', 'Lithuania', 'Luxembourg',
                   'Malta', 'Netherlands', 'Poland', 'Portugal', 'Romania', 'Slovakia', 'Slovenia', 'Spain', 'Sweden']
-
-
-def moving_average(data, window_size, axis):
-    # Apply moving average along the first axis (rows)
-    smoothed_data = np.apply_along_axis(lambda x: np.convolve(x, np.ones(window_size) / window_size, mode='valid'),
-                                        axis=axis, arr=data)
-    return smoothed_data
-
-def create_fts_years_list():
-    years_fts = list(
-        np.linspace(start=2025, stop=2050, num=6).astype(int))
-    return years_fts
-
-
-def create_ots_years_list(years_setting, astype):
-    startyear: int = years_setting[0]  # Start year is argument [0], i.e., 1990
-    baseyear: int = years_setting[1]  # Base/Reference year is argument [1], i.e., 2015
-    lastyear: int = years_setting[2]  # End/Last year is argument [2], i.e., 2050
-    step_fts = years_setting[3]  # Timestep for scenario is argument [3], i.e., 5 years
-    years_ots = list(
-        np.linspace(start=startyear, stop=baseyear, num=(baseyear - startyear) + 1).astype(int).astype(astype))
-    return years_ots
 
 
 def get_pop_eurostat(code_pop, EU27_cntr_list, dict_iso2, years_ots):
@@ -224,9 +202,84 @@ def get_pop_eurostat_fts(code_pop_fts, EU27_cntr_list, years_fts, dict_iso2):
     return dict_dm_pop_fts, dict_dm_pop_fts_tot
 
 
-years_setting = [1990, 2023, 2050, 5]  # Set the timestep for historical years & scenarios
-years_ots = create_ots_years_list(years_setting, astype=int)
-years_fts = create_fts_years_list()
+def get_household_nb_people_eustat(dict_iso2):
+
+    ##### Extract number of household with a certain number of people
+    filter = {'geo\TIME_PERIOD': list(dict_iso2.keys())}
+    mapping_dim = {'Country': 'geo\TIME_PERIOD',
+                   'Variables': 'unit',
+                   'Categories1': 'n_person'}
+    dm_ppl = get_data_api_eurostat('cens_hndwsize', filter, mapping_dim, 'households', years_ots)
+
+
+    # There is only one value in GE6 for Cyprus, but Cyprus also has 6 and GE7 data. remove GE6 and Total
+    dm_ppl.drop(col_label=['GE6', 'TOTAL'], dim='Categories1')
+    # Rename GE7 as 7
+    dm_ppl.rename_col('GE7', '7', dim='Categories1')
+    ppl_int = np.array([int(ppl) for ppl in dm_ppl.col_labels['Categories1']])
+    # Compute total number of households
+    arr_hh_tot = np.sum(dm_ppl.array, axis=-1)
+    # household-size x nb_household / tot nb households = avg household-size
+    arr_ppl_w_hh = np.sum(dm_ppl.array[:, :, :, :] * ppl_int[np.newaxis, np.newaxis, np.newaxis, :], axis=-1)
+    # Sum together all houses (since it is a nan sum I will overwrite it with the actual sum)
+    dm_ppl.group_all('Categories1')
+    # Compute average household size in number of people
+    arr_hh_size = arr_ppl_w_hh / arr_hh_tot
+    dm_ppl.add(arr_hh_size, dim='Variables', col_label='lfs_household-size', unit='people')
+    dm_ppl.rename_col('PER', 'lfs_households', dim='Variables')
+    # replace household size with avg household size
+    dm_ppl.fill_nans(dim_to_interp='Years')
+    # Number of households
+    idx = dm_ppl.idx
+    dm_ppl.array[:, :, idx['lfs_households']] = arr_hh_tot[:, :, 0]
+
+    return dm_ppl
+
+
+def get_household_size(eustat_code, dict_iso2):
+
+    ##### Household-size data eurostat
+    filter = {'geo\TIME_PERIOD': list(dict_iso2.keys())}
+    mapping_dim = {'Country': 'geo\TIME_PERIOD',
+                   'Variables': 'unit'}
+    dm_hs = get_data_api_eurostat(eustat_code, filter, mapping_dim, unit='people', years=years_ots)
+    dm_hs.rename_col('AVG', 'household-size', dim='Variables')
+
+    for i in range(2):
+        window_size = 3  # Change window size to control the smoothing effect
+        data_smooth = moving_average(dm_hs.array, window_size, axis=dm_hs.dim_labels.index('Years'))
+        dm_hs.array[:, 1:-1, ...] = data_smooth
+
+    # Treat Slovakia differently because of specific trend
+    # Constant extrapolation instead of linear fitting
+    dm_hs_slovakia = dm_hs.filter({'Country': ['Slovakia']})
+    dm_hs.drop('Country', 'Slovakia')
+    dm_hs_slovakia.fill_nans('Years')
+    dm_hs.append(dm_hs_slovakia, dim='Country')
+
+    linear_fitting(dm_hs, years_ots=years_ots)
+
+    dm_hs.sort('Country')
+
+    return dm_hs
+
+
+def estimate_household_size_fts_from_ots(dm_ots, start_t):
+
+    dm_fts_BAU = linear_forecast_BAU(dm_ots, start_t, years_ots, years_fts)
+
+    # Household-size is actually a fxa, all levels have the same value
+    dict_fts = dict()
+    for level in range(4):
+        level = level + 1
+        dict_fts[level] = dm_fts_BAU
+
+    return dict_fts
+
+
+# Set the timestep for historical years & scenarios
+years_ots = create_years_list(start_year=1990, end_year=2023, step=1, astype=int)
+years_fts = create_years_list(start_year=2025, end_year=2050, step=5, astype=int)
 
 f = open('../../country_codes_iso2.json')
 dict_iso2 = json.load(f)
@@ -252,4 +305,21 @@ if update_pop:
     for lev, dm_fts in dict_dm_pop_fts_tot.items():
         update_database_from_dm(dm_fts, filename=file, lever='pop', level=lev, module='lifestyles')
 
-# Update household size (see buildings_preprocessing_EU.py)
+# Update household size
+update_household_size = True
+if update_household_size:
+    # OTS
+    dm_household_size = get_household_size('ilc_lvph01', dict_iso2)
+    # FTS dict
+    dict_hs_fts = estimate_household_size_fts_from_ots(dm_household_size, start_t=2012)
+    # Update csv file
+    file = 'lifestyles_floor-intensity.csv'
+    # csv_database_reformat(file)
+    update_database_from_dm(dm_household_size, filename=file, lever='floor-intensity', level=0, module='lifestyles')
+    for lev, dm_fts in dict_hs_fts.items():
+        update_database_from_dm(dm_fts, filename=file, lever='floor-intensity', level=lev, module='lifestyles')
+
+
+# Household size is a fixed assumption and not an ots
+
+print('Hello')
