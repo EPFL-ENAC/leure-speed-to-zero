a27db186de3cc5ae5d950997592ffdd7a329b98a
edoardochiarotti <62812416+edoardochiarotti@users.noreply.github.com>
2024-10-23 16:30:45 +0200

    Update interactions.py
    excluding industry ammonia and minerals as they are being modified

--- model/interactions.py
+++ model/interactions.py
@@ -2,14 +2,14 @@
 from model.transport_module import transport
 from model.lifestyles_module import lifestyles
 from model.buildings_module import buildings
-from model.minerals_module import minerals
+# from model.minerals_module import minerals
 from model.common.interface_class import Interface
 from model.district_heating_module import district_heating
 from model.agriculture_module import agriculture
 from model.emissions_module import emissions
 from model.climate_module import climate
-from model.ammonia_module import ammonia
-from model.industry_module import industry
+# from model.ammonia_module import ammonia
+# from model.industry_module import industry
 from model.power_module import power
 from model.landuse_module import land_use
 from model.oilrefinery_module import refinery
@@ -44,15 +44,15 @@ def runner(lever_setting, global_vars, output_nodes, logger):
     TPE['buildings'] = buildings(lever_setting, years_setting, interface)
     logger.info('Execution time Buildings: {0:.3g} s'.format(time.time() - start_time))
     start_time = time.time()
-    TPE['industry'] = industry(lever_setting, years_setting, interface)
-    logger.info('Execution time Industry: {0:.3g} s'.format(time.time() - start_time))
-    start_time = time.time()
+    # TPE['industry'] = industry(lever_setting, years_setting, interface)
+    # logger.info('Execution time Industry: {0:.3g} s'.format(time.time() - start_time))
+    # start_time = time.time()
     TPE['agriculture'] = agriculture(lever_setting, years_setting, interface)
     logger.info('Execution time Agriculture: {0:.3g} s'.format(time.time() - start_time))
     start_time = time.time()
-    TPE['ammonia'] = ammonia(lever_setting, years_setting, interface)
-    logger.info('Execution time Ammonia: {0:.3g} s'.format(time.time() - start_time))
-    start_time = time.time()
+    # TPE['ammonia'] = ammonia(lever_setting, years_setting, interface)
+    # logger.info('Execution time Ammonia: {0:.3g} s'.format(time.time() - start_time))
+    # start_time = time.time()
     TPE['power'] = power(lever_setting, years_setting, interface)
     logger.info('Execution time Power: {0:.3g} s'.format(time.time() - start_time))
     start_time = time.time()
@@ -65,9 +65,9 @@ def runner(lever_setting, global_vars, output_nodes, logger):
     TPE['land-use'] = land_use(lever_setting, years_setting, interface)
     logger.info('Execution time Land-use: {0:.3g} s'.format(time.time() - start_time))
     start_time = time.time()
-    TPE['minerals'], TPE['minerals_EU'] = minerals(interface)
-    logger.info('Execution time Minerals: {0:.3g} s'.format(time.time() - start_time))
-    start_time = time.time()
+    # TPE['minerals'], TPE['minerals_EU'] = minerals(interface)
+    # logger.info('Execution time Minerals: {0:.3g} s'.format(time.time() - start_time))
+    # start_time = time.time()
     TPE['emissions'] = emissions(lever_setting, years_setting, interface)
     logger.info('Execution time Emissions: {0:.3g} s'.format(time.time() - start_time))
     start_time = time.time()
