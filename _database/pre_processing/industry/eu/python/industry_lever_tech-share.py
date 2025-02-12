7b7b6172b6843e3ac7ec7b1a16470cbb50a211c1
edoardochiarotti <62812416+edoardochiarotti@users.noreply.github.com>
2025-02-12 15:54:11 +0100

    Create industry_lever_tech-share.py

---
+++ _database/pre_processing/industry/eu/python/industry_lever_tech-share.py
@@ -0,0 +1,113 @@
+
+# packages
+from model.common.data_matrix_class import DataMatrix
+from model.common.auxiliary_functions import linear_fitting, fix_jumps_in_dm
+import pandas as pd
+import pickle
+import os
+import numpy as np
+import warnings
+import eurostat
+# from _database.pre_processing.api_routine_Eurostat import get_data_api_eurostat
+warnings.simplefilter("ignore")
+import plotly.express as px
+import plotly.io as pio
+pio.renderers.default='browser'
+
+# file
+__file__ = "/Users/echiarot/Documents/GitHub/2050-Calculators/PathwayCalc/_database/pre_processing/industry/eu/python/industry_lever_material-recovery.py"
+
+# directories
+current_file_directory = os.path.dirname(os.path.abspath(__file__))
+
+# get data
+filepath = os.path.join(current_file_directory, '../data/EUCalc/eucalc_technology_share.xlsx')
+df = pd.read_excel(filepath)
+
+# get current values
+df = df.iloc[list(range(2,len(df))),[0,2]]
+df.columns = ["material-tech","value"]
+
+# rename
+name_new = ['steel-BF-BOF', 'steel-scrap-EAF', 'steel-hisarna', 'steel-hydrog-DRI',
+            'cement-dry-kiln', 'cement-wet-kiln', 'cement-geopolym',
+            'chem-chem-tech', 'ammonia-tech',
+            'paper-woodpulp', 'paper-recycled',
+            'aluminium-prim', 'aluminium-sec',
+            'glass-glass','lime-lime','copper-tech']
+df["material-tech"] = name_new
+
+# add missing
+df_temp = pd.DataFrame({"material-tech" : ['fbt-tech', 'mae-tech', 'ois-tech', 'textiles-tech', 'timber-tech', 'tra-equip-tech', 'wwp-tech'],
+                        "value" : [100, 100, 100, 100, 100, 100, 100]})
+df = pd.concat([df, df_temp])
+
+# divide by 100 and order
+df["value"] = df["value"]/100
+df.sort_values(["material-tech"], inplace=True)
+
+# create dm
+countries = ['Austria','Belgium','Bulgaria','Croatia','Cyprus','Czech Republic','Denmark',
+             'EU27','Estonia','Finland','France','Germany','Greece','Hungary','Ireland','Italy',
+             'Latvia','Lithuania','Luxembourg','Malta','Netherlands','Poland','Portugal',
+             'Romania','Slovakia','Slovenia','Spain','Sweden','United Kingdom']
+years = list(range(1990,2023+1,1))
+years = years + list(range(2025, 2050+1, 5))
+variabs = list(df["material-tech"])
+units = list(np.repeat("%", len(variabs)))
+units_dict = dict()
+for i in range(0, len(variabs)):
+    units_dict[variabs[i]] = units[i]
+index_dict = dict()
+for i in range(0, len(countries)):
+    index_dict[countries[i]] = i
+for i in range(0, len(years)):
+    index_dict[years[i]] = i
+for i in range(0, len(variabs)):
+    index_dict[variabs[i]] = i
+
+dm = DataMatrix()
+dm.col_labels = {"Country" : countries, "Years" : years, "Variables" : variabs}
+dm.units = units_dict
+dm.idx = index_dict
+dm.array = np.zeros((len(countries), len(years), len(variabs)))
+idx = dm.idx
+for i in variabs:
+    dm.array[:,:,idx[i]] = df.loc[df["material-tech"]==i,"value"]
+
+# make nan for other than EU27 for fts
+countries_oth = np.array(countries)[[i not in "EU27" for i in countries]].tolist()
+idx = dm.idx
+years = list(range(2025, 2050+1, 5))
+for c in countries_oth:
+    for y in years:
+        for v in variabs:
+            dm.array[idx[c],idx[y],idx[v]] = np.nan
+df = dm.write_df()
+
+# rename
+for i in variabs:
+    dm.rename_col(i, "technology-share_" + i, "Variables")
+
+# save
+years_ots = list(range(1990,2023+1))
+years_fts = list(range(2025,2055,5))
+dm_ots = dm.filter({"Years" : years_ots})
+dm_fts = dm.filter({"Years" : years_fts})
+DM_fts = {1: dm_fts, 2: dm_fts, 3: dm_fts, 4: dm_fts} # for now we set all levels to be the same
+DM = {"ots" : dm_ots,
+      "fts" : DM_fts}
+f = os.path.join(current_file_directory, '../data/datamatrix/lever_technology-share.pickle')
+with open(f, 'wb') as handle:
+    pickle.dump(DM, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+# df = dm.write_df()
+# df_temp = pd.melt(df, id_vars = ['Country', 'Years'], var_name='variable')
+# df_temp = df_temp.loc[df_temp["Country"].isin(["Austria","France"]),:]
+# df_temp = df_temp.loc[df_temp["Years"]==1990,:]
+# name = "temp.xlsx"
+# df_temp.to_excel("~/Desktop/" + name)
+
+
+
+
