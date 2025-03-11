cf7995fce9dac02e759191287fffd6478226bab6
edoardochiarotti <62812416+edoardochiarotti@users.noreply.github.com>
2025-03-11 15:26:23 +0100

    Creating new minerals module

---
+++ _database/pre_processing/minerals/eu/python/minerals_build-pickle.py
@@ -0,0 +1,178 @@
+
+# packages
+from model.common.data_matrix_class import DataMatrix
+from model.common.auxiliary_functions import linear_fitting, fix_jumps_in_dm, my_pickle_dump
+import pandas as pd
+import pickle
+import os
+import numpy as np
+import warnings
+import eurostat
+# from _database.pre_processing.api_routine_Eurostat import get_data_api_eurostat
+warnings.simplefilter("ignore")
+import plotly.express as px
+import plotly.io as pio
+import re
+pio.renderers.default='browser'
+import subprocess
+
+# file
+__file__ = "/Users/echiarot/Documents/GitHub/2050-Calculators/PathwayCalc/_database/pre_processing/minerals/eu/python/minerals_build-pickle.py"
+
+# directories
+current_file_directory = os.path.dirname(os.path.abspath(__file__))
+
+###############################################################################
+############################### EXECUTE SCRIPTS ###############################
+###############################################################################
+
+# subprocess.run(['python', os.path.join(current_file_directory, 'minerals_lever_material-recovery.py')])
+# subprocess.run(['python', os.path.join(current_file_directory, 'minerals_const_material-decomposition.py')])
+
+###############################################################################
+################################ BUILD PICKLE #################################
+###############################################################################
+
+# files
+files_directory = os.path.join(current_file_directory, '../data/datamatrix')
+files = os.listdir(files_directory)
+
+# create DM_minerals
+DM_ots = {}
+DM_fts = {}
+# DM_fxa = {}
+# DM_cal = {}
+CDM_const = {}
+DM_minerals = {}
+
+##################
+##### LEVERS #####
+##################
+
+# list(np.array(files)[[bool(re.search("lever", i)) for i in files]])
+lever_files = ['lever_material-recovery.pickle']
+lever_names = ['eol-material-recovery']
+
+# load dms
+for i in range(0, len(lever_files)):
+    filepath = os.path.join(current_file_directory, '../data/datamatrix/' + lever_files[i])
+    with open(filepath, 'rb') as handle:
+        DM = pickle.load(handle)
+    DM_ots[lever_names[i]] = DM["ots"]
+    DM_fts[lever_names[i]] = DM["fts"]
+
+# drop ammonia
+lever_names = ['eol-material-recovery']
+for n in lever_names:
+    DM_ots[n].drop("Categories1","ammonia")
+    for i in range(1,4+1):
+        DM_fts[n][i].drop("Categories1","ammonia")
+
+#####################
+##### CONSTANTS #####
+#####################
+
+# material decomposition
+filepath = os.path.join(current_file_directory, '../data/datamatrix/' + 'const_material-decomposition.pickle')
+with open(filepath, 'rb') as handle:
+    CDM = pickle.load(handle)
+
+# CDM_const["material-decomposition_pipe"] = CDM["bld_pipe"] # do not load pipes for dh for now as this needs to be implemented in buildings
+CDM_const["material-decomposition_floor"] = CDM["bld_floor"].filter({"Categories1" : ['floor-area-new-residential', 'floor-area-reno-residential']}) # keep only residential for now as non residential need to be implemented in buildings
+# CDM_const["material-decomposition_domapp"] = CDM["bld_domapp"] # do not load domestic appliances for now as this needs to be implemented in buildings
+CDM_const["material-decomposition_infra"] = CDM["tra_infra"]
+CDM_const["material-decomposition_veh"] = CDM["tra_veh"]
+CDM_const["material-decomposition_bat"] = CDM["tra_bat"]
+CDM_const["material-decomposition_pack"] = CDM["pack"]
+
+# drop ammonia
+lever_names = ['material-decomposition_floor', 'material-decomposition_infra',
+               'material-decomposition_pack']
+for n in lever_names:
+    CDM_const[n].drop("Categories2","ammonia")
+CDM_const['material-decomposition_veh'].drop("Categories3","ammonia")
+CDM_const['material-decomposition_bat'].drop("Categories3","ammonia")
+
+########################
+##### PUT TOGETHER #####
+########################
+
+DM_minerals = {
+    'fts': DM_fts,
+    'ots': DM_ots,
+    "constant" : CDM_const
+}
+
+##########################
+##### KEEP ONLY EU27 #####
+##########################
+
+for key in DM_minerals["ots"].keys():
+    DM_minerals["ots"][key].filter({"Country" : ["EU27"]},inplace=True)
+for key in DM_minerals["fts"].keys():
+    for level in list(range(1,4+1)):
+        DM_minerals["fts"][key][level].filter({"Country" : ["EU27"]},inplace=True)
+
+
+#######################################
+###### GENERATE FAKE SWITZERLAND ######
+#######################################
+
+for key in ['ots']:
+    dm_names = list(DM_minerals[key])
+    for name in dm_names:
+        dm_temp = DM_minerals[key][name]
+        if "Switzerland" not in dm_temp.col_labels["Country"]:
+            idx = dm_temp.idx
+            arr_temp = dm_temp.array[idx["EU27"],...]
+            dm_temp.add(arr_temp[np.newaxis,...], "Country", "Switzerland")
+            dm_temp.sort("Country")
+
+
+dm_names = list(DM_minerals["fts"])
+for name in dm_names:
+    for i in range(1,4+1):
+        dm_temp = DM_minerals["fts"][name][i]
+        if "Switzerland" not in dm_temp.col_labels["Country"]:
+            idx = dm_temp.idx
+            arr_temp = dm_temp.array[idx["EU27"],...]
+            dm_temp.add(arr_temp[np.newaxis,...], "Country", "Switzerland")
+            dm_temp.sort("Country")
+            
+################################
+###### GENERATE FAKE VAUD ######
+################################
+
+for key in ['ots']:
+    dm_names = list(DM_minerals[key])
+    for name in dm_names:
+        dm_temp = DM_minerals[key][name]
+        if "Vaud" not in dm_temp.col_labels["Country"]:
+            idx = dm_temp.idx
+            arr_temp = dm_temp.array[idx["EU27"],...]
+            dm_temp.add(arr_temp[np.newaxis,...], "Country", "Vaud")
+            dm_temp.sort("Country")
+
+
+dm_names = list(DM_minerals["fts"])
+for name in dm_names:
+    for i in range(1,4+1):
+        dm_temp = DM_minerals["fts"][name][i]
+        if "Vaud" not in dm_temp.col_labels["Country"]:
+            idx = dm_temp.idx
+            arr_temp = dm_temp.array[idx["EU27"],...]
+            dm_temp.add(arr_temp[np.newaxis,...], "Country", "Vaud")
+            dm_temp.sort("Country")
+
+################
+##### SAVE #####
+################
+
+# # save
+# f = os.path.join(current_file_directory, '../../../../data/datamatrix/minerals.pickle')
+# my_pickle_dump(DM_minerals, f)
+
+# save
+f = os.path.join(current_file_directory, '../../../../data/datamatrix/minerals_new.pickle')
+with open(f, 'wb') as handle:
+    pickle.dump(DM_minerals, handle, protocol=pickle.HIGHEST_PROTOCOL)
---
+++ _database/pre_processing/minerals/eu/python/minerals_const_material-decomposition.py
@@ -0,0 +1,301 @@
+
+# packages
+from model.common.constant_data_matrix_class import ConstantDataMatrix
+import pandas as pd
+import pickle
+import os
+import warnings
+import numpy as np
+warnings.simplefilter("ignore")
+
+# file
+__file__ = "/Users/echiarot/Documents/GitHub/2050-Calculators/PathwayCalc/_database/pre_processing/minerals/eu/python/minerals_const_material-decomposition.py"
+
+# directories
+current_file_directory = os.path.dirname(os.path.abspath(__file__))
+
+#############################################
+##### NEW CONSTANTS FROM LIT REV BY E4S #####
+#############################################
+
+# get data
+filepath = os.path.join(current_file_directory, '../../../industry/eu/data/Literature/literature_review_material_decomposition.xlsx')
+df = pd.read_excel(filepath)
+
+# name first 2 columns
+df.rename(columns={"Unnamed: 0": "material", "Unnamed: 1" : "material-sub"}, inplace=True)
+
+# melt
+indexes = ["material","material-sub"]
+df = pd.melt(df, id_vars = indexes, var_name='variable')
+
+# save materials lists
+material_sub_alu = ["Cast-aluminium","Wrought-aluminium"]
+material_sub_steel = ["cast-iron","iron","steel","galvanized-steel","stainless-steel"]
+material_sub_pla = ["plastics-ABS", "plastics-PP", "plastics-PA", "plastics-PBT", "plastics-PE",
+                    "plastics-PMMA", "plastics-POM", "plastics-EPMD", "plastics-EPS", "plastics-PS",
+                    "plastics-PU", "plastics-PUR", "plastics-PET", "plastics-PVC", 
+                    "plastics-carbon-fiber-reinforced", "plastics-glass-fiber-reinforced",
+                    "plastics-mixture","plastics-other"]
+
+def aggregate_materials(df, variable):
+    
+    # get df for one variable
+    df_temp = df.loc[df["variable"] == variable,:]
+
+    # drop na in value
+    df_temp = df_temp.dropna(subset=['value'])
+
+    # rename missing material with sub material and drop sub material
+    df_temp.loc[df_temp["material"].isnull(),"material"] = df_temp.loc[df_temp["material"].isnull(),"material-sub"]
+    df_temp = df_temp.loc[:,["material","variable","value"]]
+
+    # aggregate sub materials if any
+    df_temp.loc[df_temp["material"].isin(material_sub_pla),"material"] = "plastics-total"
+    df_temp.loc[df_temp["material"].isin(material_sub_alu),"material"] = "Aluminium"
+    df_temp.loc[df_temp["material"].isin(material_sub_steel),"material"] = "iron_&_steel"
+    df_temp = df_temp.groupby(["material","variable"], as_index=False)['value'].agg(sum)
+
+    # return
+    return df_temp
+
+variabs = df["variable"].unique()
+df_agg = pd.concat([aggregate_materials(df, variable) for variable in variabs])
+
+# check
+df_check = df_agg.groupby(["variable"], as_index=False)['value'].agg(sum)
+
+# map to products we have in the calc (by taking the mean across products)
+dict_map = {"LDV_ICE-gasoline[kg/num]" : ["LDV_ICE-gasoline[kg/unit]"],
+            "LDV_ICE-diesel[kg/num]" : ["LDV_ICE-diesel[kg/unit]"],
+            "LDV_PHEV-gasoline[kg/num]" : ["LDV_HEV[kg/unit]"],
+            "LDV_BEV[kg/num]" : ["LDV_BEV[kg/unit]"],
+            "LDV_FCEV[kg/num]" : ["LDV_FCEV[kg/unit]"],
+            "HDV_ICE-diesel[kg/num]" : ["HDVH_ICE-Class-8-day-cab-truck[kg/unit]",
+                                        "HDVH_ICE-Class-8-sleeper-cab-truck[kg/unit]",
+                                        "HDVM_ICE-Class-6-PnD-truck[kg/unit]"],
+            "HDV_PHEV-diesel[kg/num]" : ["HDVH_HEV-Class-8-day-cab-truck[kg/unit]",
+                                         "HDVH_HEV-Class-8-sleeper-cab-truck[kg/unit]",
+                                         "HDVM_HEV-Class-6-PnD-truck[kg/unit]"],
+            "HDV_BEV[kg/num]" : ["HDVH_BEV-Class-8-day-cab-truck[kg/unit]",
+                                 "HDVH_BEV-Class-8-sleeper-cab-truck[kg/unit]",
+                                 "HDVM_EV-Class-6-PnD-truck[kg/unit]"],
+            "HDV_FCEV[kg/num]" : ["HDVH_FCEV-Class-8-day-cab-truck[kg/unit]",
+                                  "HDVH_FCV-Class-8-sleeper-cab-truck[kg/unit]",
+                                  "HDVM_FCV-Class-6-PnD-truck[kg/unit]"],
+            "computer[kg/num]" : ["electronics_PC[kg/unit]"],
+            "dryer[kg/num]" : ["larger-appliances_dryer[kg/unit]"],
+            "tv[kg/num]" : ["electronics_TV[kg/unit]"],
+            "phone[kg/num]" : ["electronics_phones[kg/unit]"],
+            "fridge[kg/num]" : ["larger-appliances_fridge[kg/unit]"],
+            "dishwasher[kg/num]" : ["larger-appliances_dishwasher[kg/unit]"],
+            "wmachine[kg/num]" : ["larger-appliances_washing-machine[kg/num]"], 
+            "freezer[kg/num]" : ["larger-appliances_freezer[kg/num]"], 
+            "floor-area-new-residential[kg/m2]" : ["floor-area-new-residential[kg/m2]"],
+            "floor-area-new-non-residential[kg/m2]" : ["floor-area-new-non-residential[kg/m2]"], 
+            "floor-area-reno-residential[t/m2]" : ["floor-area-reno-residential[t/m2]"], 
+            "floor-area-reno-non-residential[t/m2]" : [],	
+            "new-dhg-pipe[t/km]" : ["District heating pipes [t/km]"],	
+            "ships_ICE-diesel[t/num]" : ["Ships [t/num]"], 
+            "trains_CEV[t/num]" : ["Trains [t/num]"],
+            "planes_ICE[t/num]" : ["Planes [t/num]"],
+            "road[t/km]" : ["Road [t/km]"], 
+            "rail[t/km]" : ["Rail [t/km]"], 	
+            "trolley-cables[t/km]" : ["Trolley-cables [t/km]"],
+            "fertilizer[t/t]" : ["Fertilizer [t/t]"],
+            "plastic-pack[t/t]" : ["Plastic packaging [t/t]"],
+            "paper-pack[t/t]" : ["Paper packaging [t/t]"],
+            "aluminium-pack[t/t]" : ["Aluminium packaging [t/t]"],
+            "glass-pack[t/t]" : ["Glass packaging [t/t]"],
+            "paper-print[t/t]" : ["Paper printing and graphic [t/t]"],
+            "paper-san[t/t]" : ["Paper sanitary and household [t/t]"],
+            "battery-pbac-HDV[kg/num]" : ['Battery Pb-Ac_HDVL[kg/unit]'], 
+            "battery-pbac-LDV[kg/num]" : ['Battery Pb-Ac_LDV[kg/unit]'],
+            "battery-nimh-HDV_BEV[kg/num]" : ['Battery Ni-MH_HDVL_EV[kg/unit]'],
+            "battery-nimh-HDV_PHEV[kg/num]" : ['Battery Ni-MH_HDVL_PHEV[kg/unit]'],
+            "battery-nimh-LDV_BEV[kg/num]" : ['Battery Ni-MH_LDV_EV[kg/unit]'],
+            "battery-nimh-LDV_PHEV[kg/num]" : ['Battery Ni-MH_LDV_PHEV[kg/unit]'],
+            "battery-lion-HDV_BEV[kg/num]" : ['Battery Li-Ion-HDVL_EV[kg/unit]'],
+            "battery-lion-HDV_PHEV[kg/num]" : ['Battery Li-Ion-HDVL_PHEV[kg/unit]'],
+            "battery-lion-LDV_BEV[kg/num]" : ["Battery Li-Ion-LDV_EV[kg/unit]"],
+            "battery-lion-LDV_PHEV[kg/num]" : ["Battery Li-Ion-LDV_PHEV[kg/unit]"],
+            }
+
+for key in dict_map.keys():
+    df_agg.loc[df_agg["variable"].isin(dict_map[key]),"variable"] = key
+df_agg = df_agg.groupby(["variable","material"], as_index=False)['value'].agg(np.mean)
+
+# check
+df_check = df_agg.groupby(["variable"], as_index=False)['value'].agg(sum)
+
+# drop batteries that are not libs for now
+idx = df_agg["variable"].isin(['battery-nimh_HDV_BEV[kg/num]', 'battery-nimh_HDV_PHEV[kg/num]',
+                               'battery-nimh_LDV_BEV[kg/num]', 'battery-nimh_LDV_PHEV[kg/num]',
+                               'battery-pbac_HDV[kg/num]', 'battery-pbac_LDV[kg/num]'])
+df_agg = df_agg.loc[~idx,:]
+
+# fix units
+df_agg.loc[df_agg["variable"] == "floor-area-new-residential[kg/m2]","value"] = \
+    df_agg.loc[df_agg["variable"] == "floor-area-new-residential[kg/m2]","value"] / 1000
+df_agg.loc[df_agg["variable"] == "floor-area-new-non-residential[kg/m2]","value"] = \
+    df_agg.loc[df_agg["variable"] == "floor-area-new-non-residential[kg/m2]","value"] / 1000
+df_agg.loc[df_agg["variable"] == "floor-area-new-residential[kg/m2]","variable"] = "floor-area-new-residential[t/m2]"
+df_agg.loc[df_agg["variable"] == "floor-area-new-non-residential[kg/m2]","variable"] = "floor-area-new-non-residential[t/m2]"
+import re
+variabs = df_agg["variable"].unique()
+ls_temp = list(np.array(variabs)[[bool(re.search("kg",i)) for i in variabs]])
+ls_temp1 = [i.replace("kg","t") for i in ls_temp]
+for i in range(0, len(ls_temp)):
+    df_agg.loc[df_agg["variable"] == ls_temp[i],"value"] = \
+        df_agg.loc[df_agg["variable"] == ls_temp[i],"value"] / 1000
+    df_agg.loc[df_agg["variable"] == ls_temp[i],"variable"] = ls_temp1[i]
+# df_agg["variable"].unique()
+
+# fix material names
+materials_fix = {"Aluminium" : "aluminium", "carbon paper" : "carbon-paper",
+                 "concrete-and-inert" : "cement", "iron_&_steel" : "iron-and-steel",
+                 "plastics-total" : "plastics", 'silver ' : 'silver', "wood" : "timber"}
+for key in materials_fix.keys():
+    df_agg.loc[df_agg["material"] == key,"material"] = materials_fix[key]
+df_agg.sort_values(["variable","material"],inplace=True)
+
+# make datamatrixes
+def create_constant(df, variables):
+    
+    df_temp = df.loc[df["variable"].isin(variables),:]
+
+    # rename variables
+    df_temp["variable"] = [v.split("[")[0] + "_" + m + "[" + v.split("[")[1] for v, m in zip(df_temp["variable"],df_temp["material"])]
+    df_temp.drop(["material"], axis=1, inplace=True)
+    
+    # put unit
+    df_temp["unit"] = [i.split("[")[1].split("]")[0] for i in df_temp["variable"]]
+    
+    const = {
+        'name': list(df_temp['variable']),
+        'value': list(df_temp['value']),
+        'idx': dict(zip(list(df_temp['variable']), range(len(df_temp['variable'])))),
+        'units': dict(zip(list(df_temp['variable']), list(df_temp['unit'])))
+    }
+    
+    # return
+    return const
+
+# cdm_bld_floor
+tmp = create_constant(df_agg, ["floor-area-new-residential[t/m2]", "floor-area-new-non-residential[t/m2]",
+                               "floor-area-reno-residential[t/m2]", "floor-area-reno-non-residential[t/m2]"])
+cdm_bld_floor = ConstantDataMatrix.create_from_constant(tmp, 1)
+cdm_check = cdm_bld_floor.group_all("Categories1",inplace=False)
+df_check = cdm_check.write_df()
+
+# cdm_bld_pipe
+tmp = create_constant(df_agg, ["new-dhg-pipe[t/km]"])
+cdm_bld_pipe = ConstantDataMatrix.create_from_constant(tmp, 1)
+cdm_check = cdm_bld_pipe.group_all("Categories1",inplace=False)
+df_check = cdm_check.write_df()
+
+# cdm_domapp
+tmp = create_constant(df_agg, ["fridge[t/num]", "dishwasher[t/num]","wmachine[t/num]", 
+                               "freezer[t/num]", "dryer[t/num]", "tv[t/num]", 
+                               "phone[t/num]", "computer[t/num]"])
+cdm_domapp = ConstantDataMatrix.create_from_constant(tmp, 1)
+cdm_check = cdm_domapp.group_all("Categories1",inplace=False)
+df_check = cdm_check.write_df()
+
+# cdm_tra_veh
+variabs = df_agg["variable"].unique()
+variabs = list(np.array(variabs)[[bool(re.search("HDV|LDV|planes|trains|ships",i)) for i in variabs]])
+variabs = list(np.array(variabs)[[not bool(re.search("battery",i,re.IGNORECASE)) for i in variabs]])
+tmp = create_constant(df_agg, variabs)
+cdm_tra_veh = ConstantDataMatrix.create_from_constant(tmp, 1)
+
+# add missing veh
+# TODO: check the literature and re do the material decomp for these missing veh
+idx = cdm_tra_veh.idx
+cdm_tra_veh.add(cdm_tra_veh.array[idx["HDV_BEV"],:], "Variables", "bus_BEV", unit="t/num")
+cdm_tra_veh.add(cdm_tra_veh.array[idx["HDV_FCEV"],:], "Variables", "bus_FCEV", unit="t/num")
+cdm_tra_veh.add(cdm_tra_veh.array[idx["HDV_ICE-diesel"],:], "Variables", "bus_ICE-diesel", unit="t/num")
+cdm_tra_veh.add(cdm_tra_veh.array[idx["HDV_PHEV-diesel"],:], "Variables", "bus_PHEV-diesel", unit="t/num")
+cdm_tra_veh.add(cdm_tra_veh.array[idx["HDV_ICE-diesel"],:], "Variables", "HDV_ICE-gas", unit="t/num")
+cdm_tra_veh.add(cdm_tra_veh.array[idx["LDV_ICE-gasoline"],:], "Variables", "LDV_ICE-gas", unit="t/num")
+cdm_tra_veh.add(cdm_tra_veh.array[idx["LDV_PHEV-gasoline"],:], "Variables", "LDV_PHEV-diesel", unit="t/num")
+cdm_tra_veh.add(cdm_tra_veh.array[idx["bus_ICE-diesel"],:], "Variables", "bus_ICE-gas", unit="t/num")
+cdm_tra_veh.add(cdm_tra_veh.array[idx["HDV_PHEV-diesel"],:], "Variables", "HDV_PHEV-gasoline", unit="t/num")
+cdm_tra_veh.add(cdm_tra_veh.array[idx["HDV_ICE-diesel"],:], "Variables", "HDV_ICE-gasoline", unit="t/num")
+cdm_tra_veh.add(cdm_tra_veh.array[idx["bus_ICE-diesel"],:], "Variables", "bus_ICE-gasoline", unit="t/num")
+cdm_tra_veh.add(cdm_tra_veh.array[idx["bus_PHEV-diesel"],:], "Variables", "bus_PHEV-gasoline", unit="t/num")
+cdm_tra_veh.add(cdm_tra_veh.array[idx["trains_CEV"],:], "Variables", "trains_ICE-diesel", unit="t/num")
+cdm_tra_veh.sort("Variables")
+cdm_tra_veh.deepen(based_on="Variables")
+cdm_tra_veh.switch_categories_order("Categories1","Categories2")
+cdm_check = cdm_tra_veh.group_all("Categories1",inplace=False)
+df_check = cdm_check.write_df()
+
+# batteries
+tmp = create_constant(df_agg, ['battery-lion-HDV_BEV[t/num]', 'battery-lion-HDV_PHEV[t/num]',
+                               'battery-lion-LDV_BEV[t/num]', 'battery-lion-LDV_PHEV[t/num]'])
+cdm_tra_bat = ConstantDataMatrix.create_from_constant(tmp, 2)
+
+# cdm_tra_infra
+tmp = create_constant(df_agg, ["road[t/km]", "rail[t/km]", "trolley-cables[t/km]"])
+cdm_tra_infra = ConstantDataMatrix.create_from_constant(tmp, 1)
+cdm_check = cdm_tra_infra.group_all("Categories1",inplace=False)
+df_check = cdm_check.write_df()
+
+# cdm_fert
+tmp = create_constant(df_agg, ["fertilizer[t/t]"])
+cdm_fert = ConstantDataMatrix.create_from_constant(tmp, 1)
+cdm_check = cdm_fert.group_all("Categories1",inplace=False)
+df_check = cdm_check.write_df()
+
+# cdm_pack
+tmp = create_constant(df_agg, ["plastic-pack[t/t]", "paper-pack[t/t]", "aluminium-pack[t/t]",
+                            "glass-pack[t/t]", "paper-print[t/t]", "paper-san[t/t]"])
+cdm_pack = ConstantDataMatrix.create_from_constant(tmp, 1)
+cdm_check = cdm_pack.group_all("Categories1",inplace=False)
+df_check = cdm_check.write_df()
+
+# put together
+CDM_matdec = {
+    "pack" : cdm_pack,
+    "tra_veh" : cdm_tra_veh,
+    "tra_bat" : cdm_tra_bat,
+    "tra_infra" : cdm_tra_infra,
+    "bld_floor" : cdm_bld_floor,
+    "bld_pipe" : cdm_bld_pipe,
+    "bld_domapp" : cdm_domapp,
+    "fertilizer" : cdm_fert
+    }
+
+# rename
+for key in ['pack', 'tra_infra', 'bld_floor', 'bld_pipe', 'bld_domapp', 'fertilizer']:
+    variabs = CDM_matdec[key].col_labels["Variables"]
+    for v in variabs:
+        CDM_matdec[key].rename_col(v, "material-decomp_" + v, "Variables")
+    CDM_matdec[key] = CDM_matdec[key].flatten()
+    CDM_matdec[key].deepen_twice()
+
+for key in ["tra_veh","tra_bat"]:
+    variabs = CDM_matdec[key].col_labels["Variables"]
+    for v in variabs:
+        CDM_matdec[key].rename_col(v, "material-decomp_" + v, "Variables")
+    CDM_matdec[key] = CDM_matdec[key].flatten()
+    CDM_matdec[key].deepen(based_on="Variables")
+    CDM_matdec[key].switch_categories_order("Categories1","Categories2")
+    CDM_matdec[key].deepen(based_on="Categories2")
+
+# drop other
+# note: in general we drop other as we do not have a general technology for other materials
+# we could keep "other" and use it in industry module until the technology part, though we would need to adjust
+# net import to add other raw materials ... we'll do it only if we decide
+# to add a general tech for other at some point.
+for key in ['pack', 'tra_infra', 'bld_floor', 'bld_pipe', 'bld_domapp', 'fertilizer']:
+    CDM_matdec[key].drop("Categories2","other")
+CDM_matdec["tra_veh"].drop("Categories3","other")
+CDM_matdec["tra_bat"].drop("Categories3","other")
+
+# save
+f = os.path.join(current_file_directory, '../data/datamatrix/const_material-decomposition.pickle')
+with open(f, 'wb') as handle:
+    pickle.dump(CDM_matdec, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
