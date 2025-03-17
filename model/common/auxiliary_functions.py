1c914f81d8f5d24f9ac360d9317a2bcf78617142
PaolaLeure <124268758+PaolaLeure@users.noreply.github.com>
2025-03-17 17:04:31 +0100

    Clean for run

--- model/common/auxiliary_functions.py
+++ model/common/auxiliary_functions.py
@@ -274,7 +274,6 @@ def read_level_data(DM, lever_setting):
         if isinstance(DM['ots'][lever], dict):
             DM_ots_fts[lever] = {}
             for group in DM['ots'][lever].keys():
-                print(lever)
                 dm = DM['ots'][lever][group]
                 dm_fts = DM['fts'][lever][group][level_value]
                 dm.append(dm_fts, dim='Years')
