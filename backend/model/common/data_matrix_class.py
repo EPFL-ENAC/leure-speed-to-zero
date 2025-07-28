caa874c513ed3d2dff058549de55d7305efd709f
PaolaLeure <124268758+PaolaLeure@users.noreply.github.com>
2025-07-28 16:37:11 +0200

    Update data_matrix_class.py
    Remove unnecessary import

--- backend/model/common/data_matrix_class.py
+++ backend/model/common/data_matrix_class.py
@@ -1,10 +1,8 @@
 import numpy as np
 import re
 import pandas as pd
-import matplotlib.pyplot as plt
 import plotly.express as px
-import itertools
-import copy
+
 
 # DataMatrix is the by-default class used by the calculator.
 # DataMatrix contains:
