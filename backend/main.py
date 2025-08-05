605aa23437169e6610f3b54cdd41bf3d95c5b0f9
edoardochiarotti <62812416+edoardochiarotti@users.noreply.github.com>
2025-08-05 10:49:40 +0200

    Update main.py
    Adding ammonia

--- backend/main.py
+++ backend/main.py
@@ -14,8 +14,7 @@ lever_setting = json.load(f)[0]
 years_setting = [1990, 2023, 2025, 2050, 5]
 
 country_list = [RegionConfig.get_current_region()]
-sectors = ['climate', 'lifestyles', 'buildings', 'transport', 'industry', 'agriculture', 'forestry']
-
+sectors = ['climate', 'lifestyles', 'buildings', 'transport', 'industry', 'agriculture', 'forestry', 'ammonia']
 # Filter geoscale
 # from database/data/datamatrix/.* reads the pickles, filters the geoscale, and loads them
 DM_input = filter_country_and_load_data_from_pickles(country_list= country_list, modules_list = sectors)
