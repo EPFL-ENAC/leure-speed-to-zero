7b2acd3c6df49ae593b02fbe96b805e852681537
PaolaLeure <124268758+PaolaLeure@users.noreply.github.com>
2025-08-28 09:18:51 +0200

    Buildings pre-processing floor-area EP2050 calib
    Most of the changes are indentation, otherwise the goal is to download all cantons to then calibrate with CH

--- backend/_database/pre_processing/buildings/Switzerland/processors/floor_area_pipeline_CH.py
+++ backend/_database/pre_processing/buildings/Switzerland/processors/floor_area_pipeline_CH.py
@@ -2,19 +2,41 @@ import numpy as np
 import pickle
 import os
 
-from model.common.auxiliary_functions import create_years_list, load_pop
+from model.common.auxiliary_functions import create_years_list, load_pop, dm_add_missing_variables
 
-from _database.pre_processing.buildings.Switzerland.get_data_functions.floor_area_CH import (
-  compute_floor_area_stock_v2, extract_bld_new_buildings_2,
-  compute_bld_floor_area_new, extract_bld_new_buildings_1, compute_waste,
-  compute_floor_area_waste_cat, compute_floor_area_new_cat)
+import _database.pre_processing.buildings.Switzerland.get_data_functions.floor_area_CH as fla
 
 from _database.pre_processing.buildings.Switzerland.get_data_functions.construction_period_param import load_construction_period_param
 
+def fill_missing_years_using_FSO_data(dm, dm_raw):
 
-def run(dm_pop, global_vars, country_list, years_ots):
+  dm_add_missing_variables(dm, {'Years': dm_raw.col_labels['Years']}, fill_nans=False)
+  dm.filter({'Years': dm_raw.col_labels['Years']}, inplace=True)
+  dm_raw.rename_col('bld_floor-area_stock', 'bld_floor-area_stock_raw', dim='Variables')
+  dm.append(dm_raw, dim='Variables')
+  dm.operation('bld_floor-area_stock', '/', 'bld_floor-area_stock_raw', out_col='cal_factor', unit='%')
+  idx = dm.idx
+  dm[:, :idx[2007], 'cal_factor', :] = np.nan
+  dm.fill_nans('Years')
+  dm[:, :, 'bld_floor-area_stock', :] = dm[:, :, 'cal_factor', :] * dm[:, :, 'bld_floor-area_stock_raw', :]
+  dm.filter({'Variables': ['bld_floor-area_stock']}, inplace=True)
+
+  return dm
+
+
+def run(global_vars, country_list, years_ots):
   # Stock
 
+  cantons_en = ['Aargau', 'Appenzell Ausserrhoden', 'Appenzell Innerrhoden',
+                'Basel Landschaft', 'Basel Stadt', 'Bern', 'Fribourg', 'Geneva',
+                'Glarus', 'Graubünden', 'Jura', 'Lucerne', 'Neuchâtel',
+                'Nidwalden', 'Obwalden', 'Schaffhausen', 'Schwyz', 'Solothurn',
+                'St. Gallen', 'Thurgau', 'Ticino', 'Uri', 'Valais', 'Vaud',
+                'Zug', 'Zurich']
+
+
+  dm_pop = load_pop(cantons_en+['Switzerland'], years_ots)
+  dm_pop.sort('Country')
   # __file__ = "/Users/echiarot/Documents/GitHub/2050-Calculators/PathwayCalc/_database/pre_processing/buildings/Switzerland/buildings_preprocessing_CH.py"
   #filename = 'data/bld_household_size.pickle'
   #dm_lfs_household_size = extract_lfs_household_size(years_ots, table_id='px-x-0102020000_402', file=filename)
@@ -31,12 +53,12 @@ def run(dm_pop, global_vars, country_list, years_ots):
   # la surface du logement et l'époque de construction
   # https://www.pxweb.bfs.admin.ch/pxweb/fr/px-x-0902020200_103/-/px-x-0902020200_103.px/
   table_id = 'px-x-0902020200_103'
-  file = os.path.join(this_dir, '../data/bld_floor-area_stock.pickle')
+  file = os.path.join(this_dir, '../data/bld_floor-area_stock_all_cantons.pickle')
   #dm_bld_area_stock, dm_energy_cat = compute_bld_floor_area_stock_tranformed_avg_new_area(table_id, file,
   #                                                                years_ots, construction_period_envelope_cat_sfh,
   #                                                                construction_period_envelope_cat_mfh)
   dm_stock_tot, dm_stock_cat, dm_avg_floor_area = (
-    compute_floor_area_stock_v2(table_id, file, dm_pop=dm_pop,
+    fla.compute_floor_area_stock_v2(table_id, file, dm_pop=dm_pop,
                                 cat_map_sfh=construction_period_envelope_cat_sfh,
                                 cat_map_mfh=construction_period_envelope_cat_mfh,
                                 years_ots= years_ots))
@@ -45,27 +67,54 @@ def run(dm_pop, global_vars, country_list, years_ots):
   # New residential buildings by sfh, mfh
   # Nouveaux logements selon la grande région, le canton, la commune et le type de bâtiment, depuis 2013
   table_id = 'px-x-0904030000_107'
-  file = os.path.join(this_dir, '../data/bld_new_buidlings_2013_2023.pickle')
-  dm_bld_new_buildings_1 = extract_bld_new_buildings_1(table_id, file)
+  file = os.path.join(this_dir, '../data/bld_new_buidlings_2013_2023_all_cantons.pickle')
+  dm_bld_new_buildings_1 = fla.extract_bld_new_buildings_1(table_id, file)
 
   # Nouveaux logements selon le type de bâtiment, 1995-2012
   table_id = 'px-x-0904030000_103'
-  file = os.path.join(this_dir, '../data/bld_new_buildings_1995_2012.pickle')
-  dm_bld_new_buildings_2 = extract_bld_new_buildings_2(table_id, file)
+  file = os.path.join(this_dir, '../data/bld_new_buildings_1995_2012_all_cantons.pickle')
+  dm_bld_new_buildings_2 = fla.extract_bld_new_buildings_2(table_id, file)
   # Floor-area new by sfh, mfh
-  dm_new_tot_raw = compute_bld_floor_area_new(dm_bld_new_buildings_1, dm_bld_new_buildings_2, dm_avg_floor_area, dm_pop, years_ots)
+  dm_new_tot_raw = fla.compute_bld_floor_area_new(dm_bld_new_buildings_1, dm_bld_new_buildings_2, dm_avg_floor_area, dm_pop, years_ots)
   del dm_bld_new_buildings_2, dm_bld_new_buildings_1
 
-  # SECTION Floor-area Waste + Recompute New
-  dm_waste_tot, dm_new_tot = compute_waste(dm_stock_tot, dm_new_tot_raw, years_ots)
-  dm_waste_cat = compute_floor_area_waste_cat(dm_waste_tot)
-  # Floor-area new by sfh, mfh and envelope categories
-  dm_new_cat = compute_floor_area_new_cat(dm_new_tot, envelope_cat_new)
+  # SECTION Adjust Cantonal stock based on National stock by Prognos
+  file_url = 'https://www.bfe.admin.ch/bfe/de/home/politik/energieperspektiven-2050-plus.exturl.html/aHR0cHM6Ly9wdWJkYi5iZmUuYWRtaW4uY2gvZGUvcHVibGljYX/Rpb24vZG93bmxvYWQvMTA0NDE=.html'
+  zip_name = os.path.join(this_dir, '../data/EP2050_sectors.zip')
+  file_pickle = os.path.join(this_dir, '../data/bld_EP2050_floor_area_type.pickle')
+  dm_stock_tot_CH = fla.extract_EP2050_floor_area_by_type(file_url, zip_name, file_pickle)
 
-  DM = {'stock tot': dm_stock_tot,
-        'stock cat': dm_stock_cat,
-        'new cat': dm_new_cat,
-        'waste cat': dm_waste_cat}
+  # Find missing years in Prognos EP2050 dataset using FSO data (incl. 2000-2007 period)
+  dm_stock_tot_CH_raw = dm_stock_tot.filter({'Country': ['Switzerland']})
+  dm_stock_tot_CH = fill_missing_years_using_FSO_data(dm_stock_tot_CH, dm_stock_tot_CH_raw)
+
+  # Project to cantonal data based on shares
+  dm_stock_tot.drop('Country', 'Switzerland')
+  dm_stock_tot.normalise(dim='Country', inplace=True)
+  assert dm_stock_tot.col_labels['Categories1'] == dm_stock_tot_CH.col_labels['Categories1']
+  dm_stock_tot[...] = dm_stock_tot[...] * dm_stock_tot_CH[...]
+  dm_stock_tot.change_unit('bld_floor-area_stock', old_unit='%', new_unit='m2', factor=1)
+  dm_stock_tot.append(dm_stock_tot_CH, dim='Country')
+  dm_stock_tot.sort('Country')
+
+  # Adjust cantonal stock by energy class
+  dm_stock_cat.normalise('Categories2')
+  assert dm_stock_tot.col_labels['Categories1'] == dm_stock_cat.col_labels['Categories1']
+  assert dm_stock_tot.col_labels['Country'] == dm_stock_cat.col_labels['Country']
+  dm_stock_cat[:, :, :, :, :] = dm_stock_tot[:, :, :, :, np.newaxis] * dm_stock_cat[:, :, :, :, :]
+  dm_stock_cat.change_unit('bld_floor-area_stock', old_unit='%', new_unit='m2', factor=1)
+
+  # SECTION Floor-area Waste + Recompute New
+  dm_waste_tot, dm_new_tot = fla.compute_waste(dm_stock_tot, dm_new_tot_raw, years_ots)
+  dm_waste_cat = fla.compute_floor_area_waste_cat(dm_waste_tot)
+  # Floor-area new by sfh, mfh and envelope categories
+  dm_new_cat = fla.compute_floor_area_new_cat(dm_new_tot, envelope_cat_new)
+
+  DM = {'stock tot': dm_stock_tot.filter({'Country': country_list}),
+        'stock cat': dm_stock_cat.filter({'Country': country_list}),
+        'new cat': dm_new_cat.filter({'Country': country_list}),
+        'waste cat': dm_waste_cat.filter({'Country': country_list})}
+
 
   return DM
 
@@ -75,7 +124,12 @@ if __name__ == "__main__":
   global_vars = load_construction_period_param()
 
   years_ots = create_years_list(1990, 2023, 1)
-  country_list = ['Switzerland', 'Vaud']
-  dm_pop = load_pop(country_list, years_ots)
+  cantons_en = ['Aargau', 'Appenzell Ausserrhoden', 'Appenzell Innerrhoden',
+                'Basel Landschaft', 'Basel Stadt', 'Bern', 'Fribourg', 'Geneva',
+                'Glarus', 'Graubünden', 'Jura', 'Lucerne', 'Neuchâtel',
+                'Nidwalden', 'Obwalden', 'Schaffhausen', 'Schwyz', 'Solothurn',
+                'St. Gallen', 'Thurgau', 'Ticino', 'Uri', 'Valais', 'Vaud',
+                'Zug', 'Zurich']
+  country_list = cantons_en + ['Switzerland']
 
-  DM = run(dm_pop, global_vars, country_list, years_ots)
+  DM = run(global_vars, country_list, years_ots)
